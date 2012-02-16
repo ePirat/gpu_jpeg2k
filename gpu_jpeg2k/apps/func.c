5f824e6a02fa5d71b04877ed229f0b79684829ee
Milosz Ciznicki <miloszc@man.poznan.pl>
2012-02-16 11:16:44 +0000

    Update.
    git-svn-id: https://apps.man.poznan.pl/svn/jpeg2k/branches/libgpu_jpeg2k@57 e598733e-abc5-40e7-8b60-78866732932b

--- gpu_jpeg2k/apps/func.c
+++ gpu_jpeg2k/apps/func.c
@@ -39,14 +39,15 @@ void *_cuda_h_allocate_mem(size_t mem_size, void *ctx)
 	void *data = NULL;
 //	cuda_h_allocate_mem(&data, mem_size);
 
+	size_t aligned_size = mem_size + (DATA_ALIGMENT - (mem_size & (DATA_ALIGMENT - 1)));
 	ctx_m_t *ctx_ = (ctx_m_t *)((ctx_t *)ctx)->host;
 	data = (void *)(((uint8_t *)(ctx_->mem->p)) + ctx_->mem->alloc_size);
-	ctx_->mem->alloc_size += mem_size;
+	ctx_->mem->alloc_size += aligned_size;
 	if(ctx_->mem->alloc_size > ctx_->mem->size) {
 		perror("Insufficient memory space\n");
 		return NULL;
 	}
-	insert((t_node **)&(ctx_->head), create((uint64_t)data, mem_size));
+	insert((t_node **)&(ctx_->head), create((uint64_t)data, aligned_size));
 
 	size_t size = actual_size((t_node *)ctx_->head);
 	if(ctx_->max < size) {
