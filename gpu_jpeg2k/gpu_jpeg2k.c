91feecf77c8beabd92255c521908697254026f8b
Milosz Ciznicki <miloszc@man.poznan.pl>
2013-07-24 14:13:34 +0000

    Add license.
    git-svn-id: https://apps.man.poznan.pl/svn/jpeg2k/branches/libgpu_jpeg2k@83 e598733e-abc5-40e7-8b60-78866732932b

--- gpu_jpeg2k/gpu_jpeg2k.c
+++ gpu_jpeg2k/gpu_jpeg2k.c
@@ -1,9 +1,22 @@
-/*
- * gpu_jpeg2k.c
- *
- *  Created on: Feb 2, 2012
- *      Author: miloszc
- */
+/* 
+Copyright 2009-2013 Poznan Supercomputing and Networking Center
+
+Authors:
+Milosz Ciznicki miloszc@man.poznan.pl
+
+GPU JPEG2K is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+GPU JPEG2K is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with GPU JPEG2K. If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include "include/gpu_jpeg2k.h"
 #include "tier2/markers.h"
